f847ad9baeaba48313db57c71f0bf8edfb604930
praveenrajakumar <47105787+praveenrajakumar@users.noreply.github.com>
2019-01-28 16:22:37 +0530

    Update b1.c

--- b1.c
+++ b1.c
@@ -4,15 +4,15 @@ void main()
   int a;
   scanf("%d",&a);
   if(a>0)
-    {
+  {
   printf("postive");
-}
+  }
   else if(a<0)
   {
   printf("negative");
-}
-
-else{
-printf("zero");
-}
+  }
+  else
+  {
+  printf("zero");
+  }
 }
