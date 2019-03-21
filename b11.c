0d5f290c6f62c0596ce793a3a141dc952b9c199f
praveenrajakumar <47105787+praveenrajakumar@users.noreply.github.com>
2019-03-21 16:28:31 +0530

    Create b11.c

---
+++ b11.c
@@ -0,0 +1,13 @@
+ #include<stdio.h>
+#include<math.h>
+int main()
+{
+ int n,p,ans;
+printf("enter the num:");
+scanf("%d",&n);
+printf("enter the power:");
+scanf("%d",&p);
+ans=pow(n,p);
+printf("%d",ans);
+return 0;
+}
